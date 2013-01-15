7663871fa7d710fe9adf5de0859f0b4473a0a36c
qianlifeng <qianlf2008@163.com>
2013-01-15 13:06:00 +0800

    change access interface

--- HttpWrapper-UnitTest.py
+++ HttpWrapper-UnitTest.py
@@ -1,47 +1,41 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 import unittest
-from HttpWrapper import HttpWrapper,HttpWrapperException 
+from HttpWrapper import *
 
 class HttpWrapperTestCase(unittest.TestCase):
     def setUp(self):
-        h = HttpWrapper()
+        self.h = HttpWrapper()
         #proxy info
-        h.EnableProxyHandler({'http':'10.182.45.231:80','https':'10.182.45.231:80'})
+        self.h.EnableProxyHandler({'http':'10.182.45.231:80','https':'10.182.45.231:80'})
         #auto direct 301,302... page
-        h.EnableAutoRedirecthandler()
+        self.h.EnableAutoRedirectHandler()
         #save cookie info between requests
-        h.EnableCookieHandler()
+        self.h.EnableCookieHandler()
         #enable gzip and deflate encoding, which will reduce transmite time
-        h.EnableConetntEncodingHandler()
+        self.h.EnableConetntEncodingHandler()
 
-        r = h.Request('http://www.baidu.com',data = {},header = {})
-        r.GetContent()
+        #r = h.Request('image url')
 
-        r = h.Request('http://www.sina.com.cn')
-        r.GetContent()
-
-        r = h.Request('image url')
-
-        h.RequestAsyc('http://www.baidu.com',data = {},header = {},callback = funcname )
-        h.RequestAsyc('http://www.sina.com.cn',callback = funcname )
+        #h.RequestAsyc('http://www.baidu.com',data = {},header = {},callback = funcname )
+        #h.RequestAsyc('http://www.sina.com.cn',callback = funcname )
 
 
-        h1 = HttpWrapper()
+        #h1 = HttpWrapper()
         #h1的设定应该不和h混淆
 
 
     def PageNotFind(self):
-        r = HttpWrapper('http://www.cnblogs.com/scottqiantest')
-        assert r.GetResponseCode() == 404
+        r = self.h.Request('http://www.cnblogs.com/scottqiantest')
+        assert r.code == 404
         #print r.GetContent().decode('utf-8').encode("GB18030") #encode to GB18030 for displaying in cmd window
         #print r.GetHeaderInfo()
         #with self.assertRaises(HttpWrapperException):
             #r.GetContent()
 
     def CorrectRequest(self):
-        r = HttpWrapper('http://www.cnblogs.com')
-        assert r.GetResponseCode() == 200
+        r = self.h.Request('http://www.cnblogs.com')
+        assert r.code == 200
 
     def PostDataRequest(self):
         data = {'tbUserName'        : '1',
@@ -50,22 +44,23 @@ class HttpWrapperTestCase(unittest.TestCase):
                 '__EVENTARGUMENT'   : '',
                 '__VIEWSTATE'       : '/wEPDwULLTE1MzYzODg2NzZkGAEFHl9fQ29udHJvbHNSZXF1aXJlUG9zdEJhY2tLZXlfXxYBBQtjaGtSZW1lbWJlcm1QYDyKKI9af4b67Mzq2xFaL9Bt',
                 '__EVENTVALIDATION' : '/wEWBQLWwpqPDQLyj/OQAgK3jsrkBALR55GJDgKC3IeGDE1m7t2mGlasoP1Hd9hLaFoI2G05'}
-        r = HttpWrapper('http://passport.cnblogs.com/login.aspx',data)
-        assert r.GetContent().decode('utf-8').find(u'用户不存在') > 0
+        r = self.h.Request('http://passport.cnblogs.com/login.aspx',data)
+        assert r.content.decode('utf-8').find(u'用户不存在') > 0
     
     def RefererRequest(self):
         #tell server I'm from baidu.com
-        r = HttpWrapper('http://www.stardrifter.org/cgi-bin/ref.cgi',referer='http://www.baidu.com')
-        assert r.GetResponseCode() == 200
-        assert r.GetContent().find(u'www.baidu.com') > 0
+        headers = {'referer':'http://www.baidu.com'}
+        r = self.h.Request('http://www.stardrifter.org/cgi-bin/ref.cgi',headers=headers)
+        assert r.code == 200
+        assert r.content.find(u'www.baidu.com') > 0
 
     def AutoRedirectRequest(self):
         #auto redirect is enabled by default in HttpWrapper
-        r = HttpWrapper('http://jigsaw.w3.org/HTTP/300/301.html',enableAutoRedirect = False)
-        assert r.GetUrl() == 'http://jigsaw.w3.org/HTTP/300/Overview.html'
+        r = self.h.Request('http://jigsaw.w3.org/HTTP/300/301.html')
+        assert r.url == 'http://jigsaw.w3.org/HTTP/300/Overview.html'
 
-        r = HttpWrapper('http://jigsaw.w3.org/HTTP/300/302.html')
-        assert r.GetUrl() == 'http://jigsaw.w3.org/HTTP/300/Overview.html'
+        r = self.h.Request('http://jigsaw.w3.org/HTTP/300/302.html')
+        assert r.url == 'http://jigsaw.w3.org/HTTP/300/Overview.html'
 
 def suite():
     suite = unittest.TestSuite()
--- HttpWrapper.py
+++ HttpWrapper.py
@@ -5,11 +5,23 @@ from gzip import GzipFile
 from StringIO import StringIO
 import sys
 
-class HttpWrapperException(Exception):
+class HttpWrapperException(Exception): pass
+class HttpWrapperResponseData:
     """
-    Custom HttpWrapper Exception
+    Response data returned by HttpWrapper.Request 
     """
-    pass
+    content = None
+    url = None
+    headers = None
+    #Response http code
+    code = None
+
+    def __init__(self,content,url,headers,code):
+        self.content = content
+        self.url = url
+        self.headers
+        self.code = code
+
 
 class HttpWrapper:
   """
@@ -19,35 +31,39 @@ class HttpWrapper:
       HttpWrapper('http://xxx.com',data=dict)
   """
 
-  def __init__(self, url, postData=None,enableAutoRedirect = True,enableProxy = False,proxyDict = None,**header):
-    """
-    url                : url you want to request
-    postData           : data you want to post to the server, must be dict type,like {data1 : 'data'}
-    enableAutoRedirect : auto redirect when server return 301,302 error
-    enableProxy        : whether enable proxy
-    proxyDict          : proxy info. e.g  ProxyDict = {'http':'10.182.45.231:80','https':'10.182.45.231:80'}
-    **header           : other request info. e.g. referer='www.sina.com.cn'
-    """
-
-    self.url = url
-    self.postData =  postData
-    self.enableAutoRedirect = enableAutoRedirect
-    self.enableProxy = enableProxy
-    self.proxyDict = proxyDict
-    #tell server where i'm from,some explain about referer http://www.fwolf.com/blog/post/320
-    if 'referer' in header:
-        self.referer = header['referer']
-    else:
-        self.referer = None
-
-    if 'user-agent' in header:
-        self.user_agent = header['user-agent']
-    else:
-        self.user_agent = 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7A341 Safari/528.16'
-
-    self.__SetupHandler()
-    self.__SendRequest(self.__SetupRequest())
+  def __init__(self):
+    self.opener = urllib2.build_opener(urllib2.HTTPSHandler)
+    urllib2.install_opener(self.opener)
     
+  def EnableProxyHandler(self,proxyDict):
+    """
+    enable proxy handler
+    params:
+        proxyDict:proxy info for connect, eg. {'http':'10.182.45.231:80','https':'10.182.45.231:80'} 
+    """
+    if proxyDict == None:
+        raise HttpWrapperException('you must specify proxyDict when enabled Proxy')
+    self.opener.add_handler(urllib2.ProxyHandler(proxyDict))
+
+  def EnableAutoRedirectHandler(self):
+    """
+    enable auto redirect 301,302... pages
+    """
+    self.opener.add_handler(urllib2.HTTPRedirectHandler())
+
+  def EnableCookieHandler(self):
+    """
+    enable cookie, need this after your login
+    """
+    cj = cookielib.CookieJar()
+    self.opener.add_handler(urllib2.HTTPCookieProcessor(cj))
+
+  def EnableConetntEncodingHandler(self):
+    """
+    enable content encoding when transmite content, support gzip and deflate
+    """
+    self.opener.add_handler(self.ContentEncodingProcessor())
+
   class ContentEncodingProcessor(urllib2.BaseHandler):
     """
     A handler to add gzip capabilities to urllib2 requests 
@@ -79,120 +95,39 @@ class HttpWrapper:
       except zlib.error:
         return zlib.decompress(data)
     
-
-  def __SetupHandler(self):
+  def Request(self,url,data=None,headers={}):
     """
-    setup cookie handler, proxy handler and redirect hander for urllib2 library 
+    send a request using specified url
+    params:
+        url     : url you want to send
+        data    : additional post data
+        headers : headers you want to attach, eg. {'referer' : 'http : //www.baiud.com'}
     """
-    cj = cookielib.CookieJar()
-    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-    opener.addheaders = [('User-agent', self.user_agent)]
-    if self.enableAutoRedirect == True:
-        opener.add_handler(urllib2.HTTPRedirectHandler())
-    if self.enableProxy == True:
-        if self.proxyDict == None:
-            raise HttpWrapperException('you must specify proxyDict when enabled Proxy')
-        opener.add_handler(urllib2.ProxyHandler(self.proxyDict))
-    urllib2.install_opener(opener)
- 
-  def __SetupRequest(self):
-    """
-    setup request information
-    """
-    if self.url is None or self.url == '':
+    #setup request info
+    if url is None or url == '':
         raise HttpWrapperException("url can't be empty!")
+    if 'user-agent' not in headers:
+        headers['user-agent'] = 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7A341 Safari/528.16'
 
-    if self.postData is not None:
-        req = urllib2.Request(self.url, urllib.urlencode(self.postData))
+    if data is not None:
+        req = urllib2.Request(url, urllib.urlencode(data),headers = headers)
     else:
-        req = urllib2.Request(self.url)
+        req = urllib2.Request(url,headers = headers)
 
-    if self.referer:
-        req.add_header('referer', self.referer)
-
-    if self.user_agent:
-        req.add_header('user-agent', self.user_agent)
-
-    return req
-
-  def __SendRequest(self,req):
     try:
-      res = urllib2.urlopen(req)
-      self.source = res.read()
-      self.code = res.getcode()
-      #get real url, if we have 301 redirect page
-      self.url = res.geturl() 
-      self.head_dict = res.info().dict
-      res.close()
+        r = urllib2.urlopen(req)
+        resData = HttpWrapperResponseData(r.read(),r.geturl(),r.info().dict,r.getcode())
+        r.close()
+        return resData
     except urllib2.HTTPError,e:
-        self.code = e.code
-        self.source = e.fp.read()
-        self.head_dict = e.headers
+        return HttpWrapperResponseData(e.fp.read(),'',e.headers,e.code)
         #print e.code
         #print e.msg
         #print e.headers
         #print e.fp.read()
         #print u"error happended:\r\n Location: HttpWrapper.__SendRequest \r\n Error Information:",  sys.exc_info()[1]
 
-
-  def GetResponseCode(self):
-    """
-    get response code, e.g. 200 or 302
-    """
-    if self.code:
-        return self.code
-    return -1
-
-  def GetUrl(self):
-    if self.url:
-        return self.url
-    return None
-
-  def GetContent(self):
-    """
-    get content body of the response.
-    usually, you need to decode those content. for example, GetContent().decode('utf-8')
-    """
-    if "source" in dir(self):
-        return self.source
-    else:
-        raise HttpWrapperException(u'HttpWrapper error happended:\r\n Location: HttpWrapper.GetContent \r\n Error Information:no content find')
-
-  def GetHeaderInfo(self):
-    return self.head_dict
-
-  def GetCookie(self):
-    if 'set-cookie' in self.head_dict:
-      return self.head_dict['set-cookie']
-    else:
-      return None
-
-  def GetContentType(self):
-    if 'content-type' in self.head_dict:
-      return self.head_dict['content-type']
-    else:
-      return None
-
-  def GetCharset(self):
-    contentType = self.GetContentType()
-    if contentType is not None:
-        index = contentType.find("charset")
-        if  index > 0:
-           return contentType[index+8:]
-    return None
-
-  def GetExpiresTime(self):
-    if 'expires' in self.head_dict:
-      return self.head_dict['expires']
-    else:
-      return None
-
-  def GetServerName(self):
-    if 'server' in self.head_dict:
-      return self.head_dict['server']
-    else:
-      return None
-
 if __name__ == '__main__':
-    b = HttpWrapper('http://www.sina.com/test/fe')
-    print b.GetContent().decode('utf-8')
+    r = HttpWrapper()
+    d = r.Request('http://www.baidu.com')
+    print d.content.decode('utf-8')
