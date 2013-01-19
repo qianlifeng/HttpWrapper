8e60674974bd93be86fd677e777ad10a0be75fd1
qianlifeng <qianlf2008@163.com>
2013-01-19 15:03:35 +0800

    add HttpWrapper doc

--- HttpWrapper.py
+++ HttpWrapper.py
@@ -19,14 +19,31 @@ class HttpWrapperResponseData:
         self.url = url
         self.headers = headers
         self.code = code
+
 class HttpWrapper:
     """
-    A wrapper of http Request, integrated with cookie handler and content encoding handler(gzip)
+    A wrapper of http Request, integrated with cookie handler and content encoding handler(gzip).
+    All instance of HttpWrapper are designed to be signal, that means if you set proxy in first HttpWrapper,
+    it will not affect the second HttpWrapper instance, of course urllib2.urlopen neither.
     Usage:
-        h = HttpWrapper()
-        res = h.Request(url)
-       or:
-        res = h.Request(url,data = dict)
+        first you must init a HttpWrapper instance like this:
+            h = HttpWrapper()
+        after that, you can fetch everything you want
+            res = h.Request(url)
+        or:
+            res = h.Request(url,data = dict,headers = dict)
+
+        If you just want request header of the url,use:
+            h.RequestHeader(url)
+
+        Enable Proxy:
+            h.EnableProxyHandler({'http':'10.182.45.231:80','https':'10.182.45.231:80'})
+        Disable Proxy:
+            h.DisableProxyHandler()
+
+        sometims, maybe we don't want auto redirect(By default, AutoRedirect is enabled),use:
+            h.DisableAutoRedirectHandler()
+
     """
 
     def __init__(self):
--- README.md
+++ README.md
@@ -1,4 +1,24 @@
 HttpWrapper
 ===========
 
-a urllib2 wrapper for python
+A wrapper of http Request, integrated with cookie handler and content encoding handler(gzip).
+All instance of HttpWrapper are designed to be signal, that means if you set proxy in first HttpWrapper,
+it will not affect the second HttpWrapper instance, of course urllib2.urlopen neither.
+Usage:
+    first you must init a HttpWrapper instance like this:  
+        `h = HttpWrapper()`
+    after that, you can fetch everything you want  
+        `res = h.Request(url)`
+    or:  
+        `res = h.Request(url,data = dict,headers = dict)`
+
+    If you just want request header of the url,use:
+        `h.RequestHeader(url)`
+
+    Enable Proxy:
+        `h.EnableProxyHandler({'http':'10.182.45.231:80','https':'10.182.45.231:80'})`
+    Disable Proxy:
+        `h.DisableProxyHandler()`
+
+    sometims, maybe we don't want auto redirect(By default, AutoRedirect is enabled),use:
+        `h.DisableAutoRedirectHandler()`
